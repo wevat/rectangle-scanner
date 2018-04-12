1ef0b0efbc6add6a08d25c9037027e4a64de5bdf
Harry Bloom <harrybloom18@gmail.com>
2018-04-12 18:53:53 +0530

    Add yolo scan preset

--- RectangleScanner/Classes/Model/RectanglePresets.swift
+++ RectangleScanner/Classes/Model/RectanglePresets.swift
@@ -10,6 +10,7 @@ import Foundation
 public enum RectanglePreset {
     
     case receipt
+    case yolo
 }
 
 public extension RectanglePreset {
@@ -17,7 +18,14 @@ public extension RectanglePreset {
     func scanConfig() -> RectangleScanConfiguration {
         switch self {
         case .receipt:
-            return RectangleScanConfiguration(minimumAspectRatio: 0.1, minimumSize: 0.2, minimumConfidence: 0.9)
+            return RectangleScanConfiguration(minimumAspectRatio: 0.1,
+                                              minimumSize: 0.2,
+                                              minimumConfidence: 0.9)
+        case .yolo:
+            return RectangleScanConfiguration(minimumAspectRatio: 0,
+                                              maximumAspectRatio: 1,
+                                              minimumSize: 0.1,
+                                              minimumConfidence: 0.1)
         }
     }
 }
