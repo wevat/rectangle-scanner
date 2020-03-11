7a5dc1787e13d9552176803f1595b074ed98f5e2
Jacob Mitchinson <jake@wevat.com>
2020-03-11 12:28:54 +0100

    Add carthage compatibility (#2)
    * Add carthage compatibility
    
    * Update code signing
    
    * Unshare example project to avoid building
    
    * Update README.md
    
    * Update to 1.0.6 for consistency with Carthage

---
+++ RectangleScanner/RectangleScanner.h
@@ -0,0 +1,19 @@
+//
+//  RectangleScanner.h
+//  RectangleScanner
+//
+//  Created by Jacob Mitchinson on 21/06/2019.
+//  Copyright © 2019 Wevat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RectangleScanner.
+FOUNDATION_EXPORT double RectangleScannerVersionNumber;
+
+//! Project version string for RectangleScanner.
+FOUNDATION_EXPORT const unsigned char RectangleScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RectangleScanner/PublicHeader.h>
+
+
