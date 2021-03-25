6aa9bb6d16af3b229005b8992a54a1f211f5b392
Harry Bloom <harrybloom18@gmail.com>
2021-03-25 18:12:01 +0100

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RectangleScanner",
+    platforms: [.iOS(.v9)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "RectangleScanner",
+            targets: ["RectangleScanner"]),
+    ],
+    dependencies: [],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "RectangleScanner",
+            dependencies: []),
+    ]
+)
